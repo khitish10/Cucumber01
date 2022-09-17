38b59d9abd861350526d771520e936cc57faeed1
khitish <khitish10@gmail.com>
2022-09-17 23:58:54 +0530

    Added new dependency and test runner options

--- Cucumber_BDD/src/test/java/myTestRunner/TestRunner.java
+++ Cucumber_BDD/src/test/java/myTestRunner/TestRunner.java
@@ -7,8 +7,14 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features="Features"//specify the folder name of feature
-		,glue="stepDefinitions"//specify the package name of step definition	
+		features="Features",//specify the folder name of feature
+		glue="stepDefinitions",//specify the package name of step definition	
+		
+		dryRun=false,//verifies for each feature corresponding step definition is there or not(default value is false)
+		monochrome=true,//gives detailed logs
+		plugin={"pretty",
+                "json:target/cucumber-report/cucumber.json",
+                "html:target/cucumber-report/cucumber.html"}
 		)
 public class TestRunner {
 
